9656564927765c951bfc935df265c43079f7463d
arsuhinars <arsuhinars@gmail.com>
2023-11-10 18:14:51 +0400

    navigation package update

--- src/main/java/org/catncode/leaders_backend/navigation/service/GeocodeService.java
+++ src/main/java/org/catncode/leaders_backend/navigation/service/GeocodeService.java
@@ -1,8 +1,8 @@
 package org.catncode.leaders_backend.navigation.service;
 
 import org.catncode.leaders_backend.core.exception.AppException;
-import org.catncode.leaders_backend.navigation.dto.Point;
+import org.catncode.leaders_backend.navigation.entity.Location;
 
 public interface GeocodeService {
-    Point findPointByAddress(String address) throws AppException;
+    Location createLocationByAddress(String address) throws AppException;
 }
--- src/main/java/org/catncode/leaders_backend/navigation/service/RoutingService.java
+++ src/main/java/org/catncode/leaders_backend/navigation/service/RoutingService.java
@@ -1,12 +1,9 @@
 package org.catncode.leaders_backend.navigation.service;
 
+import org.catncode.leaders_backend.core.exception.AppException;
 import org.catncode.leaders_backend.navigation.dto.PathDistance;
 import org.catncode.leaders_backend.navigation.entity.Location;
 
-public interface DistanceMatrixService {
-    PathDistance getDistanceBetween(Location a, Location b);
-
-    void recalculateMatrix();
-
-    void recalculateFor(Location location);
+public interface RoutingService {
+    PathDistance findPath(Location start, Location end) throws AppException;
 }
